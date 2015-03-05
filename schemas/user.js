9d97d8007b496a5dba98b1b4e35c8a49d136567e
xueqian <xueqian0321@outlook.com>
2015-03-05 14:35:16 +0800

    add schemas/user.js

---
+++ schemas/user.js
@@ -0,0 +1,52 @@
+/**
+ * Created by qxue on 3/5/15.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    name: String,
+    age: Number,
+    sex: String,
+    birth: Date,
+    meta:{
+        createAt:{
+            type:Date,
+            Default:Date.now()
+        },
+        updateAt:{
+            type:Date,
+            Default:Date.now()
+        }
+    }
+});
+
+UserSchema.pre('save',function(next){
+    if(this.isNew){
+        this.meta.createAt = this.meta.updateAt = Date.now();
+    }else{
+        this.meta.updateAt = Date.now();
+    }
+    next();
+});
+
+UserSchema.statics = {
+    fetch:function(cb){
+        return this
+            .find({})
+        .sort('meta.updateAt')
+            .exec(cb)
+    },
+    findById:function(id,cb){
+        return this
+            .findOne({_id:id})
+            .exec(cb)
+    }
+};
+
+module.exports = UserSchema;
+
+
+
+
+
