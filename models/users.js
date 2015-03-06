8bcbfad9902cf91841bb9d257c5793e2248b6ad0
xueqian <xueqian0321@outlook.com>
2015-03-06 13:44:26 +0800

    modify models/users.js

--- models/users.js
+++ models/users.js
@@ -2,11 +2,13 @@
  * Created by qxue on 3/5/15.
  */
 var mongoose = require('mongoose');
-var model = mongoose.model;
+//var model = mongoose.model;
 
 var UserSchema = require('../schemas/users');
 
-var User = model('User',UserSchema);
+//var User = model('User',UserSchema);
 
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
+
+//module.exports = User;
 
