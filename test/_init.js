a5448b11c0d683908675bc6aedea0f1efa881383
xueqian <xueqian0321@outlook.com>
2015-03-09 13:41:59 +0800

    mocha test

---
+++ test/_init.js
@@ -0,0 +1,11 @@
+/**
+ * Created by qxue on 3/9/15.
+ */
+process.env.NODE_ENV = 'test';
+
+var app = require('../app')
+var chai = require('chai');
+var request = require('supertest');
+
+global.should = chai.should();
+global.request = request(app);
---
+++ test/model/user-test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//var should = require('chai').should();
+var User = require('../../model/user');
+var user;
+
+describe('User attributes',function(){
+    before(function(){
+        user = new User({
+            name: '测试'
+        });
+    });
+
+    it('name should be a string',function(){
+        user.name.should.be.a('string');
+    });
+
+});
