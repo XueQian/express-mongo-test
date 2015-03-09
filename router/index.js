20af62f3f0a50f35ce5d4272eac0a6ed2547b07a
xueqian <xueqian0321@outlook.com>
2015-03-09 16:41:10 +0800

    test routes/user-test.js

--- router/index.js
+++ router/index.js
@@ -2,5 +2,5 @@
  * Created by qxue on 3/5/15.
  */
 module.exports = function(app) {
-    app.use('/api/users', require('./routes/user'));
+    app.use('/users', require('./routes/user'));
 };
--- test/router/routes/user-test.js
+++ test/router/routes/user-test.js
@@ -1,42 +1,23 @@
-///**
-// * Created by qxue on 3/9/15.
-// */
-//'use strict';
-//
-////var User = require('../../../router/routes/user');
-////
-////describe('GET /api/users', function () {
-////    it('should return 200 OK', function (done) {
-////        request(User)
-////            .get('/api/users')
-////            .expect(200, done);
-////    });
-////});
-//
-//var request = require('supertest')
-//    , express = require('express');
-//
-//var app = express();
-////
-////app.get('/api/users', function(req, res){
-////    res.send(200, { name: 'tobi' });
-////});
-////
-////request(app)
-////    .get('/user')
-////    .expect('Content-Type', /json/)
-////    //.expect('Content-Length', '20')
-////    .expect(200)
-////    .end(function(err, res){
-////        if (err) throw err;
-////    });
-//
-//describe('GET /api/users', function(){
-//    it('respond with json', function(done){
-//        request(app)
-//            .get('/api/users')
-//            //.set('Accept', 'application/json')
-//            //.expect('Content-Type', /json/)
-//            .expect(200, done);
-//    })
-//})
+'use strict';
+
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+describe('Users', function () {
+    before(function () {
+        var user = new User({
+            name: 'Foo bar'
+        });
+    });
+
+    describe('GET /users', function () {
+        it('should respond', function (done) {
+            request
+                .get('/users')
+                .expect(200)
+                .set('Accept', 'application/json')
+                .end(done)
+        })
+    });
+
+});
