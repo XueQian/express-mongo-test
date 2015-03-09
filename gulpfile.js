4166a09971e60a954ce550659914058d4a5bc060
xueqian <xueqian0321@outlook.com>
2015-03-09 11:33:37 +0800

    configure gulp-jshint

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+/**
+ * Created by qxue on 3/7/15.
+ */
+//var gulp=require('gulp'),
+//    gutil=require('gulp-util'),
+//    uglify=require('gulp-uglify'),
+//    concat=require('gulp-concat');
+
+var jshint = require('gulp-jshint');
+var gulp   = require('gulp');
+
+gulp.task('lint_router', function() {
+    return gulp.src('./router/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('lint_model',function(){
+    return gulp.src('./model/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+//
+//gulp.task('js', function() {
+//    gulp.src('./**/*.js')
+//        .pipe(uglify())
+//        .pipe(concat('all.js'))
+//        .pipe(gulp.dest('./js'));
+//});
+//
+gulp.task('default', ['lint_router','lint_model']);
+
+//gulp.watch('./**/*.js', ['js']);
